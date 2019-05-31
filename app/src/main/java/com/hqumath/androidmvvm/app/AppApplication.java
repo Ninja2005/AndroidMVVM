3a80fef959e620f7568c0cfd2e237909568bce47
gyd <guoyadi@live.cn>
2019-05-31 17:22:31 +0800

    1.添加APP管理工具

---
+++ app/src/main/java/com/hqumath/androidmvvm/app/AppApplication.java
@@ -0,0 +1,79 @@
+package com.hqumath.androidmvvm.app;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import com.hqumath.androidmvvm.BuildConfig;
+import com.hqumath.androidmvvm.utils.KLog;
+
+/**
+ * ****************************************************************
+ * 文件名称: AppApplication
+ * 作    者: Created by gyd
+ * 创建时间: 2019/5/31 17:04
+ * 文件描述:
+ * 注意事项:
+ * 版权声明:
+ * ****************************************************************
+ */
+public class AppApplication extends Application {
+    private static Application sInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setApplication(this);
+        //是否开启打印日志
+        KLog.init(BuildConfig.DEBUG);
+        //异常捕获
+        AppCrashHandler.getInstance().init();
+    }
+
+    public static synchronized void setApplication(@NonNull Application application) {
+        sInstance = application;
+        //注册监听每个activity的生命周期,便于堆栈式管理
+        application.registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                AppManager.getInstance().addActivity(activity);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+                AppManager.getInstance().removeActivity(activity);
+            }
+        });
+    }
+
+    /**
+     * 获得当前app运行的Application
+     */
+    public static Application getInstance() {
+        if (sInstance == null) {
+            throw new NullPointerException("please inherit BaseApplication or call setApplication.");
+        }
+        return sInstance;
+    }
+}
---
+++ app/src/main/java/com/hqumath/androidmvvm/app/AppManager.java
@@ -0,0 +1,146 @@
+package com.hqumath.androidmvvm.app;
+
+import android.app.Activity;
+
+import java.util.Stack;
+
+/**
+ * ****************************************************************
+ * 文件名称: AppManager
+ * 作    者: Created by gyd
+ * 创建时间: 2019/5/31 17:06
+ * 文件描述: activity堆栈式管理
+ * 注意事项:
+ * 版权声明:
+ * ****************************************************************
+ */
+public class AppManager {
+    private static Stack<Activity> activityStack;
+    private static AppManager instance;
+
+    private AppManager() {
+    }
+
+    /**
+     * 单例模式
+     *
+     * @return AppManager
+     */
+    public static AppManager getInstance() {
+        if (instance == null) {
+            instance = new AppManager();
+        }
+        return instance;
+    }
+
+    public static Stack<Activity> getActivityStack() {
+        return activityStack;
+    }
+
+    /**
+     * 添加Activity到堆栈
+     */
+    public void addActivity(Activity activity) {
+        if (activityStack == null) {
+            activityStack = new Stack<Activity>();
+        }
+        activityStack.add(activity);
+    }
+
+    /**
+     * 移除指定的Activity
+     */
+    public void removeActivity(Activity activity) {
+        if (activity != null) {
+            activityStack.remove(activity);
+        }
+    }
+
+    /**
+     * 是否有activity
+     */
+    public boolean isActivity() {
+        if (activityStack != null) {
+            return !activityStack.isEmpty();
+        }
+        return false;
+    }
+
+    /**
+     * 获取当前Activity（堆栈中最后一个压入的）
+     */
+    public Activity currentActivity() {
+        Activity activity = activityStack.lastElement();
+        return activity;
+    }
+
+    /**
+     * 结束当前Activity（堆栈中最后一个压入的）
+     */
+    public void finishActivity() {
+        Activity activity = activityStack.lastElement();
+        finishActivity(activity);
+    }
+
+    /**
+     * 结束指定的Activity
+     */
+    public void finishActivity(Activity activity) {
+        if (activity != null) {
+            if (!activity.isFinishing()) {
+                activity.finish();
+            }
+        }
+    }
+
+    /**
+     * 结束指定类名的Activity
+     */
+    public void finishActivity(Class<?> cls) {
+        for (Activity activity : activityStack) {
+            if (activity.getClass().equals(cls)) {
+                finishActivity(activity);
+                break;
+            }
+        }
+    }
+
+    /**
+     * 结束所有Activity
+     */
+    public void finishAllActivity() {
+        for (int i = 0, size = activityStack.size(); i < size; i++) {
+            if (null != activityStack.get(i)) {
+                finishActivity(activityStack.get(i));
+            }
+        }
+        activityStack.clear();
+    }
+
+    /**
+     * 获取指定的Activity
+     *
+     * @author kymjs
+     */
+    public Activity getActivity(Class<?> cls) {
+        if (activityStack != null)
+            for (Activity activity : activityStack) {
+                if (activity.getClass().equals(cls)) {
+                    return activity;
+                }
+            }
+        return null;
+    }
+
+    /**
+     * 退出应用程序
+     */
+    public void AppExit() {
+        try {
+            finishAllActivity();
+        } catch (Exception e) {
+            activityStack.clear();
+            e.printStackTrace();
+        }
+    }
+}
