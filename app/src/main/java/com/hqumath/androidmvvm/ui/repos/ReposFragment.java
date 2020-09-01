27910e8f62992cc8ad3090afcb0f21ad264fe42f
gyd <guoyadi@live.cn>
2020-09-01 14:35:05 +0800

    1.gradle升级 2.关闭内存泄露检测 3.修改app入口 4.删除多余代码

--- app/src/main/java/com/hqumath/androidmvvm/ui/repos/ReposFragment.java
+++ app/src/main/java/com/hqumath/androidmvvm/ui/repos/ReposFragment.java
@@ -52,21 +52,5 @@ public class ReposFragment extends BaseFragment<FragmentReposBinding> {
         binding.viewpager.setAdapter(pagerAdapter);
         binding.viewpager.setOffscreenPageLimit(fragmentList.size());
         binding.tablayout.setupWithViewPager(binding.viewpager);
-        binding.tablayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                binding.viewpager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
     }
 }
